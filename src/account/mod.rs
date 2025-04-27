52c736d3365bcb8863f88438daf88a2b106a71e5
Dallin Wright <dallinwright@tradecrit.com>
2025-04-27 08:42:52 -0700

    fixed unit tests on account

--- src/account/mod.rs
+++ src/account/mod.rs
@@ -19,6 +19,7 @@ pub struct GetAccountsResponse {
 }
 
 impl Client {
+    #[tracing::instrument(skip(self))]
     pub async fn get_account(&self, account_id: &str) -> Result<Account, Error> {
         let client = self.build_authenticated_client().await?;
 
@@ -28,19 +29,18 @@ impl Client {
 
         let response: Response = utils::request(request_builder).await?;
 
-        if !response.status().is_success() {
-            let broker_error: BrokerApiError = parse_response(response).await?;
-            tracing::error!("{}", broker_error);
-            return Err(Error::new(HttpError, broker_error.to_string()));
+        if response.status().is_success() {
+            let body: Account = parse_response(response).await?;
+            tracing::debug!("{:?}", body);
+            return Ok(body);
         }
 
-        let body: Account = parse_response(response).await?;
-
-        tracing::debug!("Account info: {:?}", body);
-
-        Ok(body)
+        let broker_error: BrokerApiError = parse_response(response).await?;
+        tracing::error!("{}", broker_error);
+        Err(Error::new(HttpError, broker_error.to_string()))
     }
 
+    #[tracing::instrument(skip(self))]
     pub async fn get_accounts(&self) -> Result<GetAccountsResponse, Error> {
         let client = self.build_authenticated_client().await?;
 
@@ -50,17 +50,15 @@ impl Client {
 
         let response: Response = utils::request(request_builder).await?;
 
-        if !response.status().is_success() {
-            let broker_error: BrokerApiError = parse_response(response).await?;
-            tracing::error!("{}", broker_error);
-            return Err(Error::new(HttpError, broker_error.to_string()));
+        if response.status().is_success() {
+            let body: GetAccountsResponse = parse_response(response).await?;
+            tracing::debug!("{:?}", body);
+            return Ok(body);
         }
 
-        let body: GetAccountsResponse = parse_response(response).await?;
-
-        tracing::debug!("Account info: {:?}", body);
-
-        Ok(body)
+        let broker_error: BrokerApiError = parse_response(response).await?;
+        tracing::error!("{}", broker_error);
+        Err(Error::new(HttpError, broker_error.to_string()))
     }
 }
 
@@ -68,15 +66,61 @@ impl Client {
 
 #[cfg(test)]
 mod tests {
-    use crate::Client;
+    use tracing_subscriber::fmt::format::FmtSpan;
+use crate::Client;
     use mockito::{Server};
 
+    fn setup_tracing() {
+        let _ = tracing_subscriber::fmt()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .with_target(true)
+            .with_level(true)
+            .with_thread_ids(true)
+            .with_thread_names(true)
+            .with_span_events(FmtSpan::CLOSE)
+            .with_line_number(true)
+            .with_ansi(true)
+            .with_writer(std::io::stdout)
+            .try_init();
+    }
+
     #[tokio::test]
-    async fn test_get_account_info_success() {
+    async fn test_get_account() {
+        setup_tracing();
+
         let mut server = Server::new_async().await;
 
         let _mock = server
-            .mock("GET", "/accounts/me")
+            .mock("GET", "/accounts/123")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(r#"{
+                "account_id": 123,
+                "account_number": "ACC456",
+                "entity_id": 456,
+                "name": "Test Account"
+            }"#)
+            .create_async()
+            .await;
+
+        let client = Client::new_with_token(server.url(), "test-token".into());
+
+        let result = client.get_account("123").await;
+
+        assert!(result.is_ok());
+
+        let account = result.unwrap();
+        assert_eq!(account.account_id, 123);
+    }
+
+    #[tokio::test]
+    async fn test_get_accounts() {
+        setup_tracing();
+
+        let mut server = Server::new_async().await;
+
+        let _mock = server
+            .mock("GET", "/accounts")
             .with_status(200)
             .with_header("content-type", "application/json")
             .with_body(r#"{
@@ -94,7 +138,10 @@ mod tests {
 
         let client = Client::new_with_token(server.url(), "test-token".into());
 
-        let result = client.get_account_info().await;
+        let result = client.get_accounts().await;
         assert!(result.is_ok());
+
+        let accounts_response = result.unwrap();
+        assert_eq!(accounts_response.data.len(), 1);
     }
 }
