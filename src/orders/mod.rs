05128b95b06b89f23f8263fab6a014c9d9c89b88
Dallin Wright <dallinwright@pm.me>
2025-04-28 18:17:58 -0700

    Added create orders

--- src/orders/mod.rs
+++ src/orders/mod.rs
@@ -1,4 +1,10 @@
+use crate::error::ErrorType::HttpError;
+use crate::Error;
+use crate::error::BrokerApiError;
+use crate::utils::parse_response;
+use crate::utils;
 use serde::{Deserialize, Serialize};
+use crate::Client;
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub enum RemoteOrderState {
@@ -116,18 +122,48 @@ pub enum Destination {
     XNYS, // New York Stock Exchange
 }
 
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-pub struct CreateOrderParams<'a> {
-    pub account_id: &'a str,
-    pub reference_id: Option<&'a str>,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CreateOrderParams {
+    pub account_id: String,
+    pub reference_id: String,
     pub order_type: OrderType,
     #[serde(rename = "side")]
     pub order_side: OrderSide,
-    pub quantity: &'a str, // very weird but it's a string
-    pub price: Option<&'a str>, // very weird but it's a string
-    pub stop_price: Option<&'a str>,
+    pub quantity: String,
+    pub price: Option<String>,
+    pub stop_price: Option<String>,
     pub time_in_force: Option<TimeInForce>,
-    pub symbol: &'a str,
+    pub symbol: String,
     pub symbol_format: SymbolFormat,
     pub routing_strategy: Option<StrategyType>
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CreateOrderResponse {
+    pub order_id: String,
+}
+
+
+impl Client {
+    pub async fn create_order(&self, create_order_params: CreateOrderParams) {
+        // --url https://api.clearstreet.io/studio/v2/accounts/1234abc/orders \
+        let client = self.build_authenticated_client();
+        let url = format!("{}/studio/v2/accounts/{}/orders", self.api_url, create_order_params.account_id);
+
+
+        let request_builder = client.post(&url)
+            .json(&create_order_params);
+
+        let response = utils::request(request_builder).await?;
+
+        if response.status().is_success() {
+            let body: CreateOrderResponse = parse_response(response).await?;
+            tracing::debug!("{:?}", body);
+            return Ok(body);
+        }
+
+        let broker_error: BrokerApiError = parse_response(response).await?;
+        tracing::error!("{}", broker_error);
+        Err(Error::new(HttpError, broker_error.to_string()))
+    }
 }
