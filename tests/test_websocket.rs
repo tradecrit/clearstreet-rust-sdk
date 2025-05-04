ab4f4ceed8a2fa372c09e54a4eefb5462e00692b
Dallin Wright <dallinwright@pm.me>
2025-05-04 10:40:44 -0700

    fix

--- tests/test_websocket.rs
+++ tests/test_websocket.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::sync::Arc;
 use dotenvy::dotenv;
 use futures_util::StreamExt;
 use tokio_tungstenite::tungstenite::Message;
@@ -21,50 +20,50 @@ fn setup_tracing() {
         .try_init();
 }
 
-// #[tokio::test]
-// async fn test_get_order() {
-//     dotenv().ok().unwrap_or_default();
-//     setup_tracing();
-//
-//     let options = ClientOptions {
-//         client_id: env::var("CLIENT_ID").unwrap(),
-//         client_secret: env::var("CLIENT_SECRET").unwrap(),
-//         account_id: env::var("ACCOUNT_ID").unwrap(),
-//         ..Default::default()
-//     };
-//
-//     let client = Arc::new(Client::init(options).await.expect("Failed to initialize client"));
-//
-//     let mut ws_read: WebsocketStream = client.connect_websocket().await.unwrap();
-//
-//     while let Some(msg) = ws_read.next().await {
-//         match msg {
-//             Ok(Message::Text(text)) => {
-//                 let parsed = parse_message(text);
-//
-//                 if parsed.is_ok() {
-//                     let message = parsed.unwrap();
-//
-//                     match message {
-//                         ActivityMessage::SubscribeActivityAck(ack) => {
-//                             tracing::info!("SubscribeActivityAck: {:?}", ack);
-//                         }
-//                         ActivityMessage::OrderUpdate(update) => {
-//                             tracing::info!("OrderUpdate: {:?}", update);
-//                         }
-//                         ActivityMessage::ErrorNotice(err) => {
-//                             tracing::error!("ErrorNotice: {:?}", err);
-//                         }
-//
-//                         _ => {}
-//                     }
-//                 }
-//             }
-//             _ => {
-//                 tracing::warn!("Unexpected message: {:?}", msg);
-//             }
-//         }
-//     }
-//
-//     tracing::warn!("WebSocket stream closed");
-// }
+#[tokio::test]
+async fn test_get_order() {
+    dotenv().ok().unwrap_or_default();
+    setup_tracing();
+
+    let options = ClientOptions {
+        client_id: env::var("BROKER_CLIENT_ID").unwrap(),
+        client_secret: env::var("BROKER_CLIENT_SECRET").unwrap(),
+        account_id: env::var("BROKER_ACCOUNT_ID").unwrap(),
+        ..Default::default()
+    };
+
+    let client = Client::init(options).await.expect("Failed to initialize client");
+
+    let mut ws_read: WebsocketStream = client.connect_websocket().await.unwrap();
+
+    while let Some(msg) = ws_read.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {
+                let parsed = parse_message(text);
+
+                if parsed.is_ok() {
+                    let message = parsed.unwrap();
+
+                    match message {
+                        ActivityMessage::SubscribeActivityAck(ack) => {
+                            tracing::info!("SubscribeActivityAck: {:?}", ack);
+                        }
+                        ActivityMessage::OrderUpdate(update) => {
+                            tracing::info!("OrderUpdate: {:?}", update);
+                        }
+                        ActivityMessage::ErrorNotice(err) => {
+                            tracing::error!("ErrorNotice: {:?}", err);
+                        }
+
+                        _ => {}
+                    }
+                }
+            }
+            _ => {
+                tracing::warn!("Unexpected message: {:?}", msg);
+            }
+        }
+    }
+
+    tracing::warn!("WebSocket stream closed");
+}
